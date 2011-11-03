de262c0290bf90e3dc8e866dd5ebbed160c0c4e8
GeoAmida <subbu@geodesic.com>
2011-11-03 12:10:23 +0530

    Update src/aadhaar.c

--- src/aadhaar.c
+++ src/aadhaar.c
@@ -38,6 +38,51 @@ extern unsigned char *uid_get_aes_encrypted_data (unsigned char *in, int inlen,
 		 unsigned char *key);
 extern int uid_encode_b64(unsigned char *in, int inlen, unsigned char *out, 
 			int *outlen);
+			
+/**********************************
+	Validate Aadhaar ID
+**********************************/
+int validate_uid( char *uId )
+{
+	int dMultTable[10][10] = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 0, 6, 7, 8, 9, 5},
+		{2, 3, 4, 0, 1, 7, 8, 9, 5, 6},
+		{3, 4, 0, 1, 2, 8, 9, 5, 6, 7},
+		{4, 0, 1, 2, 3, 9, 5, 6, 7, 8},
+		{5, 9, 8, 7, 6, 0, 4, 3, 2, 1},
+		{6, 5, 9, 8, 7, 1, 0, 4, 3, 2},
+		{7, 6, 5, 9, 8, 2, 1, 0, 4, 3},
+		{8, 7, 6, 5, 9, 3, 2, 1, 0, 4},
+		{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}};
+	int permTable[8][10] = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
+		{5, 8, 0, 3, 7, 9, 6, 1, 4, 2},
+		{8, 9, 1, 6, 0, 4, 3, 5, 2, 7},
+		{9, 4, 5, 3, 1, 2, 6, 8, 7, 0},
+		{4, 2, 8, 6, 5, 7, 3, 9, 0, 1},
+		{2, 7, 9, 3, 8, 0, 6, 4, 1, 5},
+		{7, 0, 4, 6, 9, 1, 3, 2, 5, 8}};
+
+	short int i=0, c=0;
+	printf("validate_uid: %d:", c);
+	for (i=0; i < 12; i++) {
+		short int ni=0, newC=0;
+
+		if (isdigit(uId[11-i]))
+			ni = uId[11-i] - '0';
+		else {
+			printf("\n");
+			return(-1);
+		}
+		newC = dMultTable[c][permTable[i%8][ni]];
+		printf("%d:", newC);
+		c = newC;
+	}
+	printf("\n");
+	if (c == 0)
+		return(0);
+	return(-1);
+}
 
 
 char * parse_expiry_data(char *timestamp, char *expiry)
